5c9908325cb7936be1e64d16f7871abc2d23ecfd
gbaranski <me@gbaranski.com>
2022-09-12 19:35:59 +0200

    fix(webapp): compilation errors with any type

--- webapp/pages/api/health-check.ts
+++ webapp/pages/api/health-check.ts
@@ -1,3 +1,5 @@
-export default function handler(req, res) {
+import {NextApiRequest, NextApiResponse} from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
     res.status(200).json({ "status": "ok" })
 }
