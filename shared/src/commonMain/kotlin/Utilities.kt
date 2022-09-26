b36e7dfd7d013bbc45d8003895fb9537fd3cf3ed
gbaranski <me@gbaranski.com>
2022-09-26 16:45:52 +0200

    docs(shared): add comment above LanguageCode

--- shared/src/commonMain/kotlin/Utilities.kt
+++ shared/src/commonMain/kotlin/Utilities.kt
@@ -9,6 +9,7 @@ import kotlin.js.JsExport
 import kotlin.math.*
 import kotlin.time.Duration
 
+// https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
 typealias LanguageCode = String
 
 const val BASE_WEBAPP_URL = "https://web.wheretopark.app"
