a7341724bb6a6141fecd517223a5b330e05e2446
gbaranski <me@gbaranski.com>
2022-10-02 16:55:42 +0200

    fix(webapp): throwing errors if env vars arent defined

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -1,16 +1,23 @@
+const getEnv = (key) => {
+    const value = process.env[key];
+    if (value == undefined)
+        throw new Error(`Environment variable ${key} is not set`);
+    return value;
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
     reactStrictMode: true,
     swcMinify: true,
     publicRuntimeConfig: {
-        MAPBOX_ACCESS_TOKEN: process.env.MAPBOX_ACCESS_TOKEN
+        MAPBOX_ACCESS_TOKEN: getEnv("MAPBOX_ACCESS_TOKEN")
     },
     serverRuntimeConfig: {
-        CLIENT_ID: process.env.CLIENT_ID,
-        CLIENT_SECRET: process.env.CLIENT_SECRET,
-        AUTHORIZATION_URL: process.env.AUTHORIZATION_URL,
-        STOREKEEPER_URL: process.env.STOREKEEPER_URL
+        CLIENT_ID: getEnv("CLIENT_ID"),
+        CLIENT_SECRET: getEnv("CLIENT_SECRET"),
+        AUTHORIZATION_URL: getEnv("AUTHORIZATION_URL"),
+        STOREKEEPER_URL: getEnv("STOREKEEPER_URL")
     }
 }
 
