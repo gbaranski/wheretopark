a25f0da9de20ec60541caac5c3ced795b8b8114a
gbaranski <me@gbaranski.com>
2022-10-26 06:56:49 +0200

    feat(webapp): basic details page

--- webapp/src/routes/parking-lot/[id]/+page.svelte
+++ webapp/src/routes/parking-lot/[id]/+page.svelte
@@ -1,8 +1,24 @@
 <script lang="ts">
-	import type { ParkingLot } from "$lib/types";
+	import { SpotType, type ParkingLot, Feature } from "$lib/types";
+	import { getCategory } from "$lib/utils";
+    import { Title, Text } from '@svelteuidev/core';
 
     export let data: {parkingLot: ParkingLot};
+    const {metadata, state} = data.parkingLot;
+    const features = metadata.features.map((feature) => Feature[feature as keyof typeof Feature]);
+    const category = getCategory(features);
 </script>
 
-<p>{data.parkingLot.metadata.name}</p>
-<p>{data.parkingLot.metadata.address}</p>
+<div class="container">
+    <Title size={26}>{metadata.name}</Title>
+    <Text size={14} weight={"semibold"}>{category}</Text>
+    <Text size={14} weight={"semibold"}>{state.availableSpots[SpotType[SpotType.CAR]]} available car spots</Text>
+
+
+</div>
+<style>
+    .container {
+        padding: 20px;
+
+    }
+</style>
