af29e7e5cff19eb5ad201e6be2b2d22f7381d781
gbaranski <me@gbaranski.com>
2022-10-26 06:56:31 +0200

    feat(webapp): calculating category of features

--- webapp/src/lib/utils.ts
+++ webapp/src/lib/utils.ts
@@ -1,3 +1,5 @@
+import { Feature } from "./types";
+
 export function capitalizeFirstLetter(s: string) {
     return s.charAt(0).toUpperCase() + s.slice(1);
 }
@@ -14,3 +16,28 @@ export function capitalizeFirstLetter(s: string) {
 //         return `${Math.round(convertDistance(distance, 'km') * 10) / 10} km`;
 //     }
 // }
+
+
+const OF_CATEGORY_FEATURES = [
+    Feature.UNCOVERED,
+    Feature.COVERED,
+    Feature.UNDERGROUND,
+];
+
+export const getCategory = (features: Feature[]): string => {
+    const ofCategoryFeatures = features.filter((f) => OF_CATEGORY_FEATURES.includes(f));
+    if (ofCategoryFeatures.length == 0) {
+        return "Unknown";
+    } else if(ofCategoryFeatures.length == 1) {
+        const category = ofCategoryFeatures[0]!;
+        if(category == Feature.COVERED) return "Covered";
+        else if(category == Feature.UNDERGROUND) return "Underground";
+        else if(category == Feature.UNCOVERED) return "Not covered";
+    } else {
+        if (features.includes(Feature.COVERED) || features.includes(Feature.UNDERGROUND)) {
+            if (features.includes(Feature.UNCOVERED)) return "Partially covered";
+            else return "Covered & Underground";
+        }
+    }
+    return "Unknown";
+}
