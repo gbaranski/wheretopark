d55310b6bdc54dace0cb973011bfef6c04f484e1
gbaranski <me@gbaranski.com>
2022-11-05 03:24:49 +0100

    fix(webapp): move material icons load from app.html to layout

--- webapp/src/routes/+layout.svelte
+++ webapp/src/routes/+layout.svelte
@@ -10,6 +10,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com"> 
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=""> 
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@600&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<meta name="author" content="Grzegorz gbaranski Barański"/>
 </svelte:head>
 
