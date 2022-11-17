61a05f55acb20ab383b103c5ec20dddf7f5e6189
gbaranski <me@gbaranski.com>
2022-11-17 15:09:49 +0100

    remove providers/tristar-kotlin

--- providers/tristar-kotlin/src/main/kotlin/Application.kt
+++
@@ -1,7 +0,0 @@
-package app.wheretopark.providers.tristar
-
-import app.wheretopark.providers.shared.startMany
-import app.wheretopark.providers.tristar.gdansk.TristarGdanskProvider
-import app.wheretopark.providers.tristar.gdynia.TristarGdyniaProvider
-
-suspend fun main() = startMany(TristarGdanskProvider(), TristarGdyniaProvider())
