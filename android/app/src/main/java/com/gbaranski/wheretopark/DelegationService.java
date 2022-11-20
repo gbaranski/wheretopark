a3e786c5c084fb24b060afaf1171e49b206907bd
gbaranski <me@gbaranski.com>
2022-11-20 17:09:21 +0100

    add android/ with TWA

---
+++ android/app/src/main/java/com/gbaranski/wheretopark/DelegationService.java
@@ -0,0 +1,18 @@
+package com.gbaranski.wheretopark;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+    }
+}
+
