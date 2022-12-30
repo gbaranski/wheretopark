930f47fa6b15ed020232e7bcdc2c341d6abcba61
gbaranski <me@gbaranski.com>
2022-12-30 03:04:30 +0100

    feat(cctv): add concurrency

--- providers/cctv/provider.go
+++ providers/cctv/provider.go
@@ -2,6 +2,8 @@ package cctv
 
 import (
 	"fmt"
+	"sync"
+	"sync/atomic"
 	"time"
 	wheretopark "wheretopark/go"
 
@@ -12,10 +14,11 @@ import (
 type Provider struct {
 	configuration Configuration
 	model         *Model
+	modelMutex    sync.Mutex
 	savePath      *string
 }
 
-func (p Provider) GetMetadata() (map[wheretopark.ID]wheretopark.Metadata, error) {
+func (p *Provider) GetMetadata() (map[wheretopark.ID]wheretopark.Metadata, error) {
 	metadatas := make(map[wheretopark.ID]wheretopark.Metadata, len(p.configuration.ParkingLots))
 
 	for _, parkingLot := range p.configuration.ParkingLots {
@@ -26,65 +29,100 @@ func (p Provider) GetMetadata() (map[wheretopark.ID]wheretopark.Metadata, error)
 	return metadatas, nil
 }
 
-func (p Provider) GetState() (map[wheretopark.ID]wheretopark.State, error) {
-	states := make(map[wheretopark.ID]wheretopark.State)
-
+func (p *Provider) ProcessCamera(parkingLot ParkingLot, cameraID int, camera ParkingLotCamera) (uint, error) {
+	id := wheretopark.GeometryToID(parkingLot.Geometry)
+	log.Info().
+		Str("name", parkingLot.Name).
+		Int("camera", cameraID).
+		Msg("processing parking lot")
 	img := gocv.NewMat()
 	defer img.Close()
-	for _, parkingLot := range p.configuration.ParkingLots {
-		availableSpots := 0
-		captureTime := time.Now()
-		id := wheretopark.GeometryToID(parkingLot.Geometry)
-		for k, camera := range parkingLot.Cameras {
-			log.Info().
-				Str("name", parkingLot.Name).
-				Int("camera", k).
-				Msg("processing parking lot")
+	stream, err := gocv.OpenVideoCapture(camera.URL)
+	if err != nil {
+		return 0, err
+	}
+	defer stream.Close()
+	captureTime := time.Now()
+	if ok := stream.Read(&img); !ok {
+		log.Error().
+			Str("name", parkingLot.Name).
+			Int("camera", cameraID).
+			Msg("unable to read stream")
+		return 0, nil
+	}
+	spotImages := ExtractSpots(img, camera.Spots)
+	defer func() {
+		for _, spotImage := range spotImages {
+			spotImage.Close()
+		}
+	}()
+	p.modelMutex.Lock()
+	predictions := p.model.PredictMany(spotImages)
+	p.modelMutex.Unlock()
+	availableSpots := 0
+	for _, prediction := range predictions {
+		if prediction > 0.5 {
+			availableSpots += 1
+		}
+	}
 
-			stream, err := gocv.OpenVideoCapture(camera.URL)
+	if p.savePath != nil {
+		basePath := fmt.Sprintf("%s/%s/%02d", *p.savePath, id, cameraID)
+		err := SavePredictions(img, basePath, captureTime, camera.Spots, predictions)
+		if err != nil {
+			log.Error().
+				Str("name", parkingLot.Name).
+				Int("camera", cameraID).
+				Msg("unable to save predictions")
+		}
+	}
+	return uint(availableSpots), nil
+}
+
+func (p *Provider) ProcessParkingLot(parkingLot ParkingLot) wheretopark.State {
+	var availableSpots uint32
+	var wg sync.WaitGroup
+	for k, camera := range parkingLot.Cameras {
+		wg.Add(1)
+		go func(k int, camera ParkingLotCamera) {
+			defer wg.Done()
+			onCameraAvailableSpots, err := p.ProcessCamera(parkingLot, k+1, camera)
 			if err != nil {
-				return nil, err
-			}
-			defer stream.Close()
-			if ok := stream.Read(&img); !ok {
 				log.Error().
 					Str("name", parkingLot.Name).
-					Int("camera", k).
-					Msg("unable to read stream")
-				continue
+					Int("camera", k+1).
+					Err(err).
+					Send()
 			}
-			spotImages := ExtractSpots(img, camera.Spots)
-			defer func() {
-				for _, spotImage := range spotImages {
-					spotImage.Close()
-				}
-			}()
-			predictions := p.model.PredictMany(spotImages)
-			for _, prediction := range predictions {
-				if prediction > 0.5 {
-					availableSpots += 1
-				}
-			}
-
-			if p.savePath != nil {
-				basePath := fmt.Sprintf("%s/%s/%02d", *p.savePath, id, k+1)
-				err := SavePredictions(img, basePath, captureTime, camera.Spots, predictions)
-				if err != nil {
-					log.Error().
-						Str("name", parkingLot.Name).
-						Int("camera", k).
-						Msg("unable to save predictions")
-				}
-			}
-		}
-		state := wheretopark.State{
-			LastUpdated: time.Now().Format(time.RFC3339),
-			AvailableSpots: map[string]uint{
-				"CAR": uint(availableSpots),
-			},
-		}
-		states[id] = state
+			atomic.AddUint32(&availableSpots, uint32(onCameraAvailableSpots))
+		}(k, camera)
 	}
+	wg.Wait()
+	return wheretopark.State{
+		LastUpdated: time.Now().Format(time.RFC3339),
+		AvailableSpots: map[string]uint{
+			"CAR": uint(availableSpots),
+		},
+	}
+}
+
+func (p *Provider) GetState() (map[wheretopark.ID]wheretopark.State, error) {
+	states := make(map[wheretopark.ID]wheretopark.State)
+	statesMutex := sync.RWMutex{}
+	var wg sync.WaitGroup
+
+	for _, parkingLot := range p.configuration.ParkingLots {
+		wg.Add(1)
+		go func(parkingLot ParkingLot) {
+			defer wg.Done()
+			id := wheretopark.GeometryToID(parkingLot.Geometry)
+			state := p.ProcessParkingLot(parkingLot)
+			statesMutex.Lock()
+			states[id] = state
+			statesMutex.Unlock()
+		}(parkingLot)
+	}
+	wg.Wait()
 	return states, nil
 }
 
@@ -103,6 +141,7 @@ func NewProvider(configurationPath *string, model *Model, savePath *string) (*Pr
 	return &Provider{
 		configuration: configuration,
 		model:         model,
+		modelMutex:    sync.Mutex{},
 		savePath:      savePath,
 	}, nil
 
