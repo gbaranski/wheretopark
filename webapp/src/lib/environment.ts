93904eb0062645245538b9baae5de98bdcbc4f0f
gbaranski <me@gbaranski.com>
2023-08-18 21:00:31 +0200

    feat: update

--- webapp/src/lib/environment.ts
+++ webapp/src/lib/environment.ts
@@ -2,6 +2,7 @@ import { env } from '$env/dynamic/public';
 
 const get = (name: string): string => {
     const key = `PUBLIC_${name}`;
+    // @ts-ignore TODO: Remove this ignore
     const v = env[key];
     if(v == null && v == undefined) {
         throw new Error(`environment variable ${key} is missing`);
@@ -10,7 +11,18 @@ const get = (name: string): string => {
 }
 
 const MAPBOX_ACCESS_TOKEN = get("MAPBOX_ACCESS_TOKEN");
+const AWS_API_KEY = get("AWS_API_KEY");
+const AWS_REGION = "eu-central-1";
+const APP_ID = "2cww2oe";
+const PROD_ENV_ID = "47osndv";
+const ENV_ID = PROD_ENV_ID;
+const CONFIG_PROFILE_ID = "8k18g1o";
 
 export {
     MAPBOX_ACCESS_TOKEN,
+    AWS_API_KEY,
+    AWS_REGION,
+    APP_ID,
+    ENV_ID,
+    CONFIG_PROFILE_ID,
 }
--- webapp/src/lib/server/client.ts
+++ webapp/src/lib/server/client.ts
@@ -1,27 +1,105 @@
-import type {ID, ParkingLot } from "../types";
-import {DATABASE_NAME, DATABASE_PASSWORD, DATABASE_URL, DATABASE_USER} from "./secrets";
-import Surreal from 'surrealdb.js'
+import {
+  APP_ID,
+  AWS_API_KEY,
+  AWS_REGION,
+  CONFIG_PROFILE_ID,
+  ENV_ID,
+} from "$lib/environment";
+import type { ID, ParkingLot } from "../types";
+import {
+  AppConfigDataClient,
+  GetLatestConfigurationCommand,
+  type GetLatestConfigurationCommandInput,
+  StartConfigurationSessionCommand,
+  type StartConfigurationSessionRequest,
+} from "@aws-sdk/client-appconfigdata";
 
-const db = new Surreal(`${DATABASE_URL}/rpc`);
-await db.signin({
-    user: DATABASE_USER,
-    pass: DATABASE_PASSWORD,
-});
-await db.use("wheretopark", DATABASE_NAME);
+type Collector = {
+  name: string;
+  url: URL;
+};
+
+type AppConfiguration = {
+  collectors: Collector[];
+};
+
+const appConfigClient = new AppConfigDataClient({ region: AWS_REGION });
+const getConfigurationToken = async (): Promise<string> => {
+  const input: StartConfigurationSessionRequest = {
+    ApplicationIdentifier: APP_ID,
+    EnvironmentIdentifier: ENV_ID,
+    ConfigurationProfileIdentifier: CONFIG_PROFILE_ID,
+    RequiredMinimumPollIntervalInSeconds: 60,
+  };
+  const command = new StartConfigurationSessionCommand(input);
+  const response = await appConfigClient.send(command);
+  if (response.InitialConfigurationToken == undefined) {
+    throw new Error("could not retrieve configuration token");
+  }
+  return response.InitialConfigurationToken;
+};
+
+const getLatestConfiguration = async (): Promise<AppConfiguration> => {
+  const configurationToken = await getConfigurationToken();
+  const params: GetLatestConfigurationCommandInput = {
+    ConfigurationToken: configurationToken,
+  };
+  const command = new GetLatestConfigurationCommand(params);
+  const data = await appConfigClient.send(command);
+  if (data.Configuration == undefined) {
+    throw new Error("configuration is empty");
+  }
+  if (data.ContentType != "application/json") {
+    throw new Error("configuration is not json");
+  }
+  const configurationString = new TextDecoder().decode(data.Configuration);
+  const configuration = JSON.parse(
+    configurationString,
+  ) as AppConfiguration;
+  return configuration;
+};
+
+const getParkingLotsFromCollector = async (
+  collector: Collector,
+): Promise<ParkingLot[]> => {
+  const response = await fetch(`${collector.url}/parking-lots`, {
+    headers: {
+      "x-api-key": AWS_API_KEY,
+    },
+  });
+  const parkingLots = await response.json();
+  return parkingLots;
+};
+
+const getParkingLotsS = async (collectors: Collector[]) => {
+  const parkingLotRequests = collectors.map(async (collector) => ({
+    collector,
+    data: await getParkingLotsFromCollector(collector),
+  }));
+  const parkingLotsByCollector = (await Promise.all(parkingLotRequests)).flat();
+  const parkingLots = parkingLotsByCollector.flatMap((e) => e.data);
+  return parkingLots;
+};
+
+const appConfig = await getLatestConfiguration();
+const parkingLots = getParkingLotsS(appConfig.collectors);
+// console.log({parkingLots});
 
 export const getParkingLots = async (): Promise<Record<ID, ParkingLot>> => {
-    const rawParkingLots = await db.select<ParkingLot & { id?: string}>("parking_lot");
-    const parkingLots = rawParkingLots.map((parkingLot) => {
-        const id = parkingLot.id!.split(":")[1]
-        delete parkingLot.id;
-        return [id, parkingLot];
-    });
-    return Object.fromEntries(parkingLots);
-}
+  return {};
+  // const rawParkingLots = await db.select<ParkingLot & { id?: string}>("parking_lot");
+  // const parkingLots = rawParkingLots.map((parkingLot) => {
+  //     const id = parkingLot.id!.split(":")[1]
+  //     delete parkingLot.id;
+  //     return [id, parkingLot];
+  // });
+  // return Object.fromEntries(parkingLots);
+};
 
 export const getParkingLot = async (id: string): Promise<ParkingLot | null> => {
-    const rawParkingLot = await db.select<ParkingLot & { id?: string}>(`parking_lot:${id}`);
-    const parkingLot = rawParkingLot[0];
-    delete parkingLot.id;
-    return parkingLot;
-}
+  return null;
+  // const rawParkingLot = await db.select<ParkingLot & { id?: string}>(`parking_lot:${id}`);
+  // const parkingLot = rawParkingLot[0];
+  // delete parkingLot.id;
+  // return parkingLot;
+};
