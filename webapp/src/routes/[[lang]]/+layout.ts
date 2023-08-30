af6923ae79101e1a871fd59a329f9859b2abc1dc
gbaranski <me@gbaranski.com>
2023-08-31 01:38:36 +0200

    fix(webapp): disable pre-render

--- webapp/src/routes/[[lang]]/+layout.ts
+++ webapp/src/routes/[[lang]]/+layout.ts
@@ -1,9 +1,6 @@
 import { updateParkingLots } from "$lib/client";
 import type { LayoutLoad } from "./$types";
 
-export const prerender = true;
-export const ssr = false;
-
 export const load = (async ({fetch}) => {
     updateParkingLots(fetch);
 }) satisfies LayoutLoad;
