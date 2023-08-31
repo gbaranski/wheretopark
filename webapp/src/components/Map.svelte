fa006e383ffaf83a495f82b619ceca25d6186430
gbaranski <me@gbaranski.com>
2023-08-31 02:40:40 +0200

    feat: api versioning, dynamic provider urls

--- webapp/src/components/Map.svelte
+++ webapp/src/components/Map.svelte
@@ -1,46 +1,56 @@
 <script lang="ts">
-    import { PUBLIC_MAPBOX_ACCESS_TOKEN } from '$env/static/public';
-	import { onMount } from "svelte";
-    import 'mapbox-gl/dist/mapbox-gl.css';
-	import { SpotType, type ID, type ParkingLot } from "$lib/types";
-	import { currentMap, parkingLots as parkingLotsStore } from "$lib/store";
-	import { markerColor, parkingLotStatus } from "$lib/utils";
-    import mapboxgl from "mapbox-gl";
-    
-    let map: mapboxgl.Map;
-    $: parkingLots = $parkingLotsStore;
-    $: {
-        Object.entries(parkingLots).map(([id, parkingLot]) => {
-            const [longitude, latitude] = parkingLot.metadata.geometry.coordinates;
-            const popupHtml = `
+	import { PUBLIC_MAPBOX_ACCESS_TOKEN } from '$env/static/public';
+	import { onMount } from 'svelte';
+	import 'mapbox-gl/dist/mapbox-gl.css';
+	import { SpotType, type ID, type ParkingLot } from '$lib/types';
+	import { currentMap, parkingLots as parkingLotsStore } from '$lib/store';
+	import { markerColor, parkingLotStatus } from '$lib/utils';
+
+	let map: mapboxgl.Map;
+	$: parkingLots = $parkingLotsStore;
+
+	onMount(async () => {
+		const mapboxgl = await import('mapbox-gl');
+		map = new mapboxgl.Map({
+			accessToken: PUBLIC_MAPBOX_ACCESS_TOKEN,
+			container: 'map-container',
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [19.21, 52.11],
+			zoom: 5
+		});
+		currentMap.set(map);
+		parkingLotsStore.subscribe((parkingLots) => {
+			Object.entries(parkingLots)
+				.map(([id, parkingLot]) => {
+					const [longitude, latitude] = parkingLot.metadata.geometry.coordinates;
+					const popupHtml = `
                 <h4>${parkingLot.metadata.name}</h4>
-                <p>${parkingLot.state.availableSpots["CAR"]} available car spots</p>
+                <p>${parkingLot.state.availableSpots['CAR']} available car spots</p>
                 <a href="/parking-lot/${id}">Open</a>
             `;
-            const popup = new mapboxgl.Popup({offset: 25}).setHTML(popupHtml);
-            const status = parkingLotStatus(parkingLot, SpotType.CAR)[0];
-            const options = {
-                color: markerColor(parkingLot.state.availableSpots["CAR"], parkingLot.metadata.totalSpots["CAR"], status)
-            }
-            return new mapboxgl.Marker(options)
-                .setLngLat([longitude, latitude])
-                .setPopup(popup);
-        }).forEach((marker) => marker.addTo(map));
-    }
-
-    onMount(async () => {
-        map = new mapboxgl.Map({
-            accessToken: PUBLIC_MAPBOX_ACCESS_TOKEN,
-            container: "map-container",
-            style: "mapbox://styles/mapbox/streets-v11",
-            center: [19.21, 52.11],
-            zoom: 5,
-        });
-        currentMap.set(map);
-    });
+					const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(popupHtml);
+					const status = parkingLotStatus(parkingLot, SpotType.CAR)[0];
+					const options = {
+						color: markerColor(
+							parkingLot.state.availableSpots['CAR'],
+							parkingLot.metadata.totalSpots['CAR'],
+							status
+						)
+					};
+					return new mapboxgl.Marker(options).setLngLat([longitude, latitude]).setPopup(popup);
+				})
+				.forEach((marker) => marker.addTo(map));
+		});
+	});
 </script>
 
 <div id="map-container"></div>
+
 <style>
-    #map-container { position: absolute; top: 0; bottom: 0; width: 100%; }
+	#map-container {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		width: 100%;
+	}
 </style>
--- webapp/src/lib/client.ts
+++ webapp/src/lib/client.ts
@@ -3,13 +3,20 @@ import { parkingLots as parkingLotsStore } from "$lib/store";
 import { dev } from '$app/environment';
 
 const serverURL = dev ? "http://localhost:1234" : "https://api.wheretopark.app";
-const providersURLs = [
-    `${serverURL}/collector`,
-    `${serverURL}/cctv`,
-];
 
-const getParkingLotFromProvider = async function* (fetch: typeof window.fetch, url: string): AsyncGenerator<Record<ID, ParkingLot>> {
-    const response = await fetch(`${url}/parking-lots`);
+type Provider = {
+    name: string;
+    url: URL;
+}
+
+const getProviders = async (fetch: typeof window.fetch): Promise<Provider[]> => {
+    const response = await fetch(`${serverURL}/v1/providers`);
+    const providers = await response.json() as Provider[];
+    return providers;
+}
+
+const getParkingLotFromProvider = async function* (fetch: typeof window.fetch, provider: Provider): AsyncGenerator<Record<ID, ParkingLot>> {
+    const response = await fetch(`${provider.url}/parking-lots`);
     const reader = response.body!.getReader();
     let buffer = '';
     const decoder = new TextDecoder("utf-8")
@@ -37,8 +44,10 @@ const getParkingLotFromProvider = async function* (fetch: typeof window.fetch, u
 }
 
 export const updateParkingLots = async (fetch: typeof window.fetch) => {
-    const promises = providersURLs.map(async (url) => {
-        const providerParkingLots = getParkingLotFromProvider(fetch, url);
+    const providers = await getProviders(fetch);
+    console.log({providers});
+    const promises = providers.map(async (provider) => {
+        const providerParkingLots = getParkingLotFromProvider(fetch, provider);
         for await (const parkingLots of providerParkingLots) {
             parkingLotsStore.update((value) => {
                 return {...value, ...parkingLots};
