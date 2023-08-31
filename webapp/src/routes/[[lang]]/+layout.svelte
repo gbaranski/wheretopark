9df80aa8eb55ae4c30dbf3ee1b2b05792b3a389d
gbaranski <me@gbaranski.com>
2023-08-31 20:10:47 +0200

    fix(webapp): restore map

--- webapp/src/routes/[[lang]]/+layout.svelte
+++ webapp/src/routes/[[lang]]/+layout.svelte
@@ -19,51 +19,49 @@ import NavbarMenu from '$components/NavbarMenu.svelte';
 	></script>
 </svelte:head>
 
-<div class="">
-	<div class="navbar absolute z-20 bg-base-100">
-		<div class="navbar-start">
-			<div class="max-md:hidden">
-				<Logo/>
-			</div>
+<div class="navbar absolute z-20 bg-base-100">
+	<div class="navbar-start">
+		<div class="max-md:hidden">
+			<Logo/>
 		</div>
-		<div class="navbar-center">
-			<div class="md:hidden">
-				<Logo/>
-			</div>
-			<ul class="menu menu-horizontal px-1 hidden md:flex">
-				<NavbarMenu />
+	</div>
+	<div class="navbar-center">
+		<div class="md:hidden">
+			<Logo/>
+		</div>
+		<ul class="menu menu-horizontal px-1 hidden md:flex">
+			<NavbarMenu />
+		</ul>
+	</div>
+	<div class="navbar-end">
+		<select class="select max-md:hidden">
+			<option selected>🇵🇱</option>
+			<option>🇬🇧</option>
+		</select>
+		<div class="dropdown dropdown-end">
+			<button tabindex="0" class="btn btn-ghost md:hidden">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h8m-8 6h16"
+					/></svg
+				>
+			</button>
+			<ul
+				class="menu menu-md dropdown-content mt-3 fixed z-20 p-2 shadow bg-base-100 rounded-box w-52"
+			>
+				<NavbarMenu languageSwitcher />
 			</ul>
 		</div>
-		<div class="navbar-end">
-			<select class="select max-md:hidden">
-				<option selected>🇵🇱</option>
-				<option>🇬🇧</option>
-			</select>
-			<div class="dropdown dropdown-end">
-				<button tabindex="0" class="btn btn-ghost md:hidden">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-5 w-5"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-						><path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M4 6h16M4 12h8m-8 6h16"
-						/></svg
-					>
-				</button>
-				<ul
-					class="menu menu-md dropdown-content mt-3 fixed z-20 p-2 shadow bg-base-100 rounded-box w-52"
-				>
-					<NavbarMenu languageSwitcher />
-				</ul>
-			</div>
-		</div>
 	</div>
-	<div class="relative top-10">
-		<slot />
-	</div>
+</div>
+<div class="pt-10">
+	<slot />
 </div>
