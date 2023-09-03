68d2cd1f647fcc5ea98c8c5d308f5e22e31c6cfe
gbaranski <me@gbaranski.com>
2023-09-03 14:35:32 +0200

    feat(webapp): remove location button next to where'd park. auto clear suggestions

--- webapp/src/components/SearchBox.svelte
+++ webapp/src/components/SearchBox.svelte
@@ -32,18 +32,19 @@
         }
 	});
 
-	const setCurrentLocation = () => {};
-
 	let searchTerm: string;
 	let timeout: NodeJS.Timeout;
 
     const onInputChange: FormEventHandler<HTMLInputElement> = (e) => {
         const target = e.target as HTMLInputElement;
         const searchTerm = target.value;
-		if (!searchFor || !searchTerm) return;
+		if (!searchFor) return;
 		clearTimeout(timeout);
 		timeout = setTimeout(async () => {
-            if (searchTerm.length < 3) return;
+            if (searchTerm.length < 3) {
+				suggestions = [];
+				return;
+			}
             
 			console.log(`searching for ${searchTerm}`);
 			searchFor(searchTerm);
@@ -51,33 +52,14 @@
     }
 </script>
 
-<div class="flex items-center">
-	<input
-		name="address"
-		type="text"
-		placeholder="🔍  Where'd you park today?"
-		class="input input-md input-primary input-bordered w-11/12 text-sm bg-inherit"
-		bind:value={searchTerm}
-        on:input={onInputChange}
-	/>
-	<button class="btn btn-md btn-outline btn-secondary ml-2" on:click={setCurrentLocation}>
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke-width="1.5"
-			stroke="currentColor"
-			class="w-6 h-6"
-		>
-			<path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-			/>
-		</svg>
-	</button>
-</div>
+<input
+	name="address"
+	type="text"
+	placeholder="🔍  Where'd you park today?"
+	class="input input-md input-primary input-bordered w-full text-sm bg-inherit block"
+	bind:value={searchTerm}
+	on:input={onInputChange}
+/>
 
 
 {#if suggestions.length > 0}
