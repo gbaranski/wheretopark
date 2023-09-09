2abdaa862f2d3f6a4e4cf5e1ddb83f35d1652f16
gbaranski <me@gbaranski.com>
2023-09-09 14:00:34 +0200

    feat(webapp): add about page

---
+++ webapp/src/routes/about/+page.svelte
@@ -0,0 +1,30 @@
+<div class="p-16 gap-6 flex flex-col lg:flex-row object-fill">
+	<div class="self-center basis-1/2">
+		<h1 class="text-2xl font-bold">Contact</h1>
+		<p>
+			Email: <a class="font-bold link link-info" href="mailto:contact@wheretopark.app"
+				>contact@wheretopark.app</a
+			>
+		</p>
+		<br />
+		<h1 class="text-2xl font-bold">Our Team</h1>
+		<ul class="list-disc">
+			<li>
+				<a href="https://gbaranski.com" target="_blank" class="link link-secondary"
+					>Gregory <code>gbaranski</code> Baranski</a
+				> - founder of Where To Park, full-stack software developer
+			</li>
+			<li>
+				<a
+					href="https://www.linkedin.com/in/aleksandra-mycan-48a363279/"
+					target="_blank"
+					class="link link-secondary"
+				>
+					Aleksandra Mycan
+				</a>
+				- CEO, Business Relations
+			</li>
+		</ul>
+	</div>
+	<img src="/assets/team.jpg" alt="our team" class="mask mask-squircle self-center lg:w-3/5" />
+</div>
--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -5,17 +5,6 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
-      animation: {
-        ["infinite-slider"]: "infiniteSlider 20s linear infinite",
-      },
-      keyframes: {
-        infiniteSlider: {
-          "0%": { transform: "translateX(0)" },
-          "100%": {
-            transform: "translateX(calc(-250px * 5))",
-          },
-        },
-      },
       fontFamily: {
         'sans': ['Poppins', ...defaultTheme.fontFamily.sans],
       }
