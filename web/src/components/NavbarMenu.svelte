f9f7e618ec732b099069311f66e832152397fc79
gbaranski <me@gbaranski.com>
2023-09-09 15:10:49 +0200

    fix(web): icon size in navbar

--- web/src/components/NavbarMenu.svelte
+++ web/src/components/NavbarMenu.svelte
@@ -13,7 +13,7 @@
 			viewBox="0 0 24 24"
 			stroke-width="1.5"
 			stroke="currentColor"
-			class="w-6 h-6"
+			class="w-8 h-8"
 		>
 			<path
 				stroke-linecap="round"
@@ -32,7 +32,7 @@
 			viewBox="0 0 24 24"
 			stroke-width="1.5"
 			stroke="currentColor"
-			class="w-6 h-6"
+			class="w-8 h-8"
 		>
 			<path
 				stroke-linecap="round"
