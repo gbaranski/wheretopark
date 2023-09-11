4665954f52be6a404526da151a3567695bd7fb83
gbaranski <me@gbaranski.com>
2023-09-11 22:51:53 +0200

    feat(app): add navbar for mobile

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -21,18 +21,20 @@
 	<meta name="keywords" content="Parking Lot, Smart City, Gdańsk, Gdynia, Sopot, Tricity" />
 </svelte:head>
 
-<div class="w-screen h-screen flex flex-col lg:flex-row">
+<div class="absolute z-30 navbar bg-base-100 lg:hidden justify-center h-16">
+	<Logo/>
+</div>
+
+<div class="w-screen h-screen flex flex-col lg:flex-row max-lg:pt-16">
 	<div class="w-full h-full lg:order-2">
 		<Map />
 	</div>
 	<div
 		class="px-5 py-3 h-1/3 lg:overflow-y-scroll lg:h-full lg:w-7/12 xl:w-5/12 2xl:w-4/12 lg:order-1"
 	>
-		{#if !isNative}
-			<div class="pb-3 text-center">
-				<Logo />
-			</div>
-		{/if}
+		<div class="pb-3 text-center max-lg:hidden">
+			<Logo />
+		</div>
 		<slot />
 	</div>
 </div>
