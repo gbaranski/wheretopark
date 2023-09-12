d02187cd794f015a61ddc4e70ef15089bbeec02a
gbaranski <me@gbaranski.com>
2023-09-12 16:25:02 +0200

    feat(website): add possible benefits

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -18,6 +18,15 @@
 			counter = 10;
 		}, 3000);
 	};
+
+	const references = {
+		krakow2018:
+			'https://yadda.icm.edu.pl/baztech/element/bwmeta1.element.baztech-51e98198-f84e-4b3f-8a4a-2492f1fef1df',
+		krakow2021:
+			'https://yadda.icm.edu.pl/baztech/element/bwmeta1.element.baztech-d8bf4176-2ea0-4571-b5a5-bdc066343c33',
+		copark:
+			'https://www.researchgate.net/publication/307534127_Investigating_Vehicle-to-Vehicle_Communication_for_Cooperative_Car_Parking_the_CoPark_Approach'
+	};
 </script>
 
 <div class="flex flex-col text-center w-full pt-24 p-12">
@@ -101,9 +110,8 @@
 			<h3 class="text-xl">
 				Our system automatically marks
 				<span class="text-green-600">green</span>
-				free spaces,
-				and 
-				<span class="text-red-600">red</span> 
+				free spaces, and
+				<span class="text-red-600">red</span>
 				occupied.
 			</h3>
 		</div>
@@ -161,6 +169,67 @@
 				/>
 			</a>
 		{/each}
+	</div>
+
+	<div class="self-center divider w-96 p-12" />
+	<div>
+		<div class="pb-10">
+			<h2 class="font-extrabold text-4xl pb-10">Benefits 📈</h2>
+			<div class="flex flex-col text-left items-center justify-evenly gap-10 lg:flex-row lg:items-start">
+				<div class="flex-1 w-3/4">
+					<h2 class="text-2xl font-bold">For drivers 🚗</h2>
+					<ul class="list-disc">
+						<li>
+							<b>Money savings</b> of around
+							<a class="link" href={references.krakow2021} target="_blank">9zł or 2$</a> per each parking
+							session (based on costs of running the car and wasted time).
+						</li>
+						<li>
+							<b>People with disabilities</b> can easily check which parking lots have specially designated
+							spaces for them, and whether these spaces are free.
+						</li>
+						<li>
+							<b>Predictions of availability</b>. The user has access to the statistics of the
+							occupancy of places on parking lots at a given time and on a given day. For example,
+							on Friday at 5 pm, the parking lot is usually 80% full, and on Saturday at 2 pm, it is
+							50% full.
+						</li>
+					</ul>
+				</div>
+				<div class="flex-1 w-3/4">
+					<h2 class="text-2xl font-bold">For parking operators 👔</h2>
+					<ul class="list-disc">
+						<li>
+							<b>In-app promotion</b> - more customers.
+						</li>
+						<li>
+							<b>Reserving parking spaces</b> directly in the app, without need of phone calling.
+						</li>
+					</ul>
+				</div>
+				<div class="flex-1 w-3/4">
+					<h2 class="text-2xl font-bold">For cities 🌇</h2>
+					<ul class="list-disc">
+						<li>
+							<b>Trend analysis</b> - will help cities choosing the right place for a new parking lot,
+							or managing the prices.
+						</li>
+						<li>
+							Our system can <b>detect illegally parked vehicle</b> and notify the authorities.
+						</li>
+						<li>
+							<b>Decreased car traffic</b> - in some cities up to
+							<a href={references.krakow2021} class="link" target="_blank">70% of traffic</a> is caused
+							by drivers.
+						</li>
+						<li>
+							<b>Optimised distribution of cars</b> - drivers could start using less known, or recently
+							opened parking lots, thus reducing the load on the most demanded ones.
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
 	</div>
 
 	<div class="self-center divider w-96 p-12" />
