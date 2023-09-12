290b8f4c0c9245b8c45940654e9a4ffd58d6d483
gbaranski <me@gbaranski.com>
2023-09-12 22:23:39 +0200

    chore(website): add configuration of adapter-static

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -8,10 +8,13 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true,
+		})
 	}
 };
 
