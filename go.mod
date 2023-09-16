76b1ddad1e5b3e547f441374bfd177f24407197b
gbaranski <me@gbaranski.com>
2023-09-16 02:50:55 +0200

    feat(go): add flush call

--- go.mod
+++ go.mod
@@ -7,11 +7,10 @@ toolchain go1.21.1
 require (
 	github.com/allegro/bigcache/v3 v3.1.0
 	github.com/caarlos0/env/v8 v8.0.0
-	github.com/gin-contrib/logger v0.2.6
-	github.com/gin-gonic/gin v1.9.1
 	github.com/go-resty/resty/v2 v2.7.0
 	github.com/goccy/go-yaml v1.11.0
 	github.com/google/go-cmp v0.5.9
+	github.com/julienschmidt/httprouter v1.3.0
 	github.com/mmcloughlin/geohash v0.10.0
 	github.com/paulmach/go.geojson v1.5.0
 	github.com/rs/zerolog v1.30.0
@@ -25,40 +24,22 @@ require (
 
 require (
 	github.com/aws/aws-sdk-go v1.38.20 // indirect
-	github.com/bytedance/sonic v1.10.0 // indirect
-	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
-	github.com/chenzhuoyu/iasm v0.9.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.15.0 // indirect
-	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
-	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/go-playground/locales v0.14.1 // indirect
-	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.15.3 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
-	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/u2takey/go-utils v0.3.1 // indirect
-	github.com/ugorji/go/codec v1.2.11 // indirect
-	golang.org/x/arch v0.5.0 // indirect
-	golang.org/x/crypto v0.12.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
-	github.com/goccy/go-json v0.10.2 // indirect
 	golang.org/x/net v0.14.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
--- go/provider.go
+++ go/provider.go
@@ -2,12 +2,12 @@ package wheretopark
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync"
 
-	"github.com/gin-contrib/logger"
-	"github.com/gin-gonic/gin"
+	"github.com/julienschmidt/httprouter"
 	"github.com/rs/zerolog/log"
 )
 
@@ -20,12 +20,35 @@ func RunProvider(p Provider, port uint) error {
 	if err != nil {
 		return fmt.Errorf("failed to create cache: %w", err)
 	}
-	r := gin.Default()
-	r.ForwardedByClientIP = true
-	r.Use(logger.SetLogger())
-	r.GET("/parking-lots", func(c *gin.Context) {
-		c.Writer.WriteHeader(http.StatusOK)
-		c.Writer.Header().Set("Content-Type", "application/json")
+
+	r := httprouter.New()
+	// r.GET("/:id/parking-lots", func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// 	identifier := ps.ByName("id")
+	// 	source := p.Sources()[identifier]
+	// 	ctx := log.With().Str("source", identifier).Logger().WithContext(context.TODO())
+	// 	parkingLots, err := source.ParkingLots(ctx)
+	// 	if err != nil {
+	// 		log.Error().Err(err).Str("identifier", identifier).Msg("get parking lots from source failure")
+	// 		w.WriteHeader(http.StatusInternalServerError)
+	// 		return
+	// 	}
+
+	// 	json, err := json.Marshal(parkingLots)
+	// 	if err != nil {
+	// 		w.WriteHeader(http.StatusInternalServerError)
+	// 		log.Error().Err(err).Str("identifier", identifier).Msg("failed to marshal parking lots")
+	// 		return
+	// 	}
+	// 	_, err = w.Write(json)
+	// 	if err != nil {
+	// 		log.Error().Err(err).Str("identifier", identifier).Msg("failed to write response")
+	// 		return
+	// 	}
+	// })
+	r.GET("/parking-lots", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		w.Header().Set("Content-Type", "application/json")
+
+		var mu sync.Mutex
 		var wg sync.WaitGroup
 		for identifier, source := range p.Sources() {
 			wg.Add(1)
@@ -39,26 +62,28 @@ func RunProvider(p Provider, port uint) error {
 					log.Error().Err(err).Str("identifier", identifier).Msg("get parking lots faliure")
 					return
 				}
+				json, err := json.Marshal(parkingLots)
+				if err != nil {
+					log.Error().Err(err).Msg("failed to marshal parking lots")
+					return
+				}
+				mu.Lock()
+				_, err = w.Write([]byte(string(json) + "\r\n"))
+				if err != nil {
+					log.Error().Err(err).Msg("failed to write response")
+					return
+				}
+				mu.Unlock()
 				log.Debug().Int("n", len(parkingLots)).Msg("sending parking lots to client")
-				c.JSON(http.StatusOK, parkingLots)
+				if f, ok := w.(http.Flusher); ok {
+					f.Flush()
+				}
 			}(identifier, source)
 		}
 		wg.Wait()
-	})
-	r.GET("/:identifier/parking-lots", func(c *gin.Context) {
-		identifier := c.Param("identifier")
-		source := p.Sources()[identifier]
-		ctx := log.With().Str("source", identifier).Logger().WithContext(context.TODO())
-		parkingLots, err := source.ParkingLots(ctx)
-		if err != nil {
-			log.Error().Err(err).Str("identifier", identifier).Msg("get parking lots from source failure")
-			c.Status(http.StatusInternalServerError)
-			return
-		}
-
-		c.JSON(http.StatusOK, parkingLots)
+		w.WriteHeader(http.StatusOK)
 	})
 
 	log.Info().Msg(fmt.Sprintf("starting server on port %d", port))
-	return r.Run(fmt.Sprintf(":%d", port))
+	return http.ListenAndServe(fmt.Sprintf(":%d", port), r)
 }
