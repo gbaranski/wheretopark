e442a3997edba4571d650e7d172be5618477f6e7
gbaranski <me@gbaranski.com>
2023-09-18 05:13:42 +0200

    fix(go): include Flush into mutex lock

--- go/provider.go
+++ go/provider.go
@@ -79,12 +79,12 @@ func GetProviderRouter(p Provider) (*httprouter.Router, error) {
 					return
 				}
 				mu.Lock()
+				defer mu.Unlock()
 				_, err = w.Write([]byte(string(json) + "\r\n"))
 				if err != nil {
 					log.Error().Err(err).Msg("failed to write response")
 					return
 				}
-				mu.Unlock()
 				log.Debug().Int("n", len(parkingLots)).Msg("sending parking lots to client")
 				if f, ok := w.(http.Flusher); ok {
 					f.Flush()
