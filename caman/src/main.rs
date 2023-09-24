e068129cb5c102041a08ef4d5d224692268793a6
gbaranski <me@gbaranski.com>
2023-09-25 01:54:34 +0200

    feat(caman): using ffmpg to download images

--- caman/src/main.rs
+++ caman/src/main.rs
@@ -1,16 +1,21 @@
 mod model;
 mod utils;
+mod stream;
 
 use image::imageops;
 use image::imageops::FilterType;
 use model::Model;
+use stream::capture;
 
 use crate::utils::visualise;
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     let model = Model::new()?;
-    let image = image::open("demo.png")?.into_rgb8();
+    let url = reqwest::Url::parse("https://cam4out.klemit.net/hls/camn826.m3u8")?;
+    let image = capture(url).await?;
+    let image = image.into_rgb8();
     let objects = model.infere(&image)?;
     let image = imageops::resize(&image, 1280, 32 * 22, FilterType::Lanczos3);
     let image = visualise(image, &objects);
---
+++ caman/src/stream.rs
@@ -0,0 +1,38 @@
+use image::io::Reader as ImageReader;
+use image::DynamicImage;
+use image::ImageFormat;
+use reqwest::Url;
+use std::io::Cursor;
+use tokio::process::Command;
+
+pub async fn capture(url: Url) -> anyhow::Result<DynamicImage> {
+    let mut command = Command::new("ffmpeg");
+    // set input URL
+    command.arg("-i");
+    command.arg(url.to_string());
+    // set to accept only the first frame
+    command.arg("-vframes");
+    command.arg("1");
+    // interpret input as image
+    command.arg("-f");
+    command.arg("image2");
+    // encode to png
+    command.arg("-c:v");
+    command.arg("png");
+    // set output to stdout
+    command.arg("-");
+
+    let output = command.output().await?;
+    if !output.status.success() {
+        return Err(anyhow::anyhow!(
+            "ffmpeg exited with status {}",
+            output.status
+        ));
+    }
+    let cursor = Cursor::new(output.stdout);
+    let mut reader = ImageReader::new(cursor);
+    reader.set_format(ImageFormat::Png);
+    let image = reader.decode()?;
+
+    Ok(image)
+}
