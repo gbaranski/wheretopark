09bd9d5fd97ad362e62dcade5911f746a17ad334
Gregory Baranski <me@gbaranski.com>
2023-10-29 15:19:07 +0200

    feat(landing): add content from prev version

---
+++ landing/src/assets/cities/index.ts
@@ -0,0 +1,43 @@
+import gdansk from "./gdansk.png";
+import gdynia from "./gdynia.png";
+import glasgow from "./glasgow.png";
+import klodzko from "./klodzko.png";
+import poznan from "./poznan.png";
+import sopot from "./sopot.png";
+import warsaw from "./warsaw.webp";
+import losAngeles from "./los-angeles.png";
+
+export const cities = [
+  {
+    name: "gdansk",
+    image: gdansk,
+  },
+  {
+    name: "sopot",
+    image: sopot,
+  },
+  {
+    name: "gdynia",
+    image: gdynia,
+  },
+  {
+    name: "warsaw",
+    image: warsaw,
+  },
+  {
+    name: "los-angeles",
+    image: losAngeles,
+  },
+  {
+    name: "glasgow",
+    image: glasgow,
+  },
+  {
+    name: "klodzko",
+    image: klodzko,
+  },
+  {
+    name: "poznan",
+    image: poznan,
+  },
+];
---
+++ landing/tailwind.config.js
@@ -0,0 +1,55 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+const colors = {
+	red: "#EF2D56",
+	green: "#04E762",
+	yellow: "#FFC15E",
+	orange: "#EC8004",
+	blue: "#2B59C3",
+	white: "#FFFFFF",
+	darkBlue: "#383F51",
+	veryDarkBlue: "#011627",
+};
+
+const colorScheme = {
+	"primary": colors.yellow,
+	"secondary": colors.darkBlue,
+	"accent": colors.red,
+	"neutral": colors.veryDarkBlue,
+	"base-100": colors.white,
+	"info": colors.blue,
+	"success": colors.green,
+	"warning": colors.orange,
+	"error": colors.red,
+};
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: colorScheme.primary,
+				secondary: colorScheme.secondary,
+				accent: colorScheme.accent,
+				default: "#101010",
+				muted: "rgba(16, 16, 16, 0.66)",
+			},
+			fontFamily: {
+				'sans': ['Inter Variable', ...defaultTheme.fontFamily.sans],
+			},
+		}
+	},
+	daisyui: {
+		themes: [
+			{
+				myTheme: colorScheme,
+			},
+		],
+	},
+	plugins: [
+		require("daisyui"),
+		require('@tailwindcss/typography'),
+	],
+}
