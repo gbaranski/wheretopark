cd0644fa35732a53f9bfd0068ce426703a44f467
Gregory Baranski <me@gbaranski.com>
2023-11-27 18:01:38 +0100

    fix(app): make logo smaller for mobile

--- app/src/lib/components/Logo.svelte
+++ app/src/lib/components/Logo.svelte
@@ -4,5 +4,5 @@
 </script>
 
 <a href="/" data-sveltekit-noscroll>
-	<img src={IconWithText} alt="where to park icon" class="object-scale-down btn btn-ghost w-full py-2 h-20 my-2" height="256px" />
+	<img src={IconWithText} alt="where to park icon" class="object-scale-down btn btn-ghost w-full py-2 h-16 md:h-20 my-2" height="256px" />
 </a>
