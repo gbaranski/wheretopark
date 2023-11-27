2a593c60d7881b39143044cff89946ff3fcc1992
Gregory Baranski <me@gbaranski.com>
2023-11-27 13:24:13 +0100

    feat(app): update logo

--- app/src/lib/components/Logo.svelte
+++ app/src/lib/components/Logo.svelte
@@ -1,25 +1,8 @@
 <script>
 	import { isLoading } from '$lib/store';
+	import IconWithText from "$lib/assets/icon-text@h256.webp"
 </script>
 
-<a href="/" class="btn btn-ghost normal-case text-xl h-16" data-sveltekit-noscroll>
-	<img src="/icon@64.webp" alt="where to park icon" class="m-1" width="40px" height="40px" />
-	<span class="logo font-bold text-2xl">
-		<span class="text-primary"> where </span>
-
-		{#if $isLoading}
-			<span class="loading loading-bars loading-md text-success align-top"></span>
-		{:else}
-			<span class="text-success text-opacity-80"> to </span>
-		{/if}
-		<span class="text-error text-opacity-80"> park </span>
-	</span>
+<a href="/" data-sveltekit-noscroll>
+	<img src={IconWithText} alt="where to park icon" class="object-scale-down btn btn-ghost w-full py-2 h-20 my-2" height="256px" />
 </a>
-
-<style>
-	@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@700&display=swap');
-
-	.logo {
-		font-family: 'Josefin Sans', sans-serif;
-	}
-</style>
