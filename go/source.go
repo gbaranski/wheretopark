13c5c620621a3c4dd328ffccc78fdce13a6e785d
Gregory Baranski <me@gbaranski.com>
2023-12-04 11:22:47 +0100

    fix(go): improve error handling for prefetch

--- go/source.go
+++ go/source.go
@@ -11,20 +11,30 @@ type Source interface {
 	ParkingLots(context.Context) (<-chan map[ID]ParkingLot, error)
 }
 
+func poll(ctx context.Context, cache *Cache, source Source) (uint, error) {
+	ch, err := source.ParkingLots(ctx)
+	if err != nil {
+		return 0, err
+	}
+	count := uint(0)
+	for parkingLots := range ch {
+		count += uint(len(parkingLots))
+		for id, parkingLot := range parkingLots {
+			cache.SetParkingLot(id, &parkingLot)
+		}
+	}
+	return count, nil
+}
+
 func RunPrefetch(cache *Cache, source Source, interval time.Duration) {
+	ctx := log.With().Logger().WithContext(context.TODO())
 	for {
-		ctx := log.With().Logger().WithContext(context.TODO())
-		ch, err := source.ParkingLots(ctx)
+		count, err := poll(ctx, cache, source)
 		if err != nil {
-			log.Error().Err(err).Msg("get parking lots failure")
-			return
+			log.Ctx(ctx).Error().Err(err).Msg("prefetch failed")
+		} else {
+			log.Ctx(ctx).Debug().Uint("parking-lots", count).Msg("prefetch succeesfull")
 		}
-		for parkingLots := range ch {
-			for id, parkingLot := range parkingLots {
-				cache.SetParkingLot(id, &parkingLot)
-			}
-		}
-		log.Ctx(ctx).Debug().Msg("prefetch done")
 		time.Sleep(interval)
 	}
 }
